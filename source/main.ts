3a7ce86a13374a1ae2b519800f3f91f07305b03d
Jon Keen <jon.statik@gmail.com>
2022-09-06 15:38:32 -0400

    pseudo code

---
+++ source/main.ts
@@ -0,0 +1,38 @@
+// Assignment layout
+// getComputerChoice() random return 'Rock', 'Paper', or 'Scissors'
+// Use console to check returns
+// 
+// function singleRound (playerSelection, computerSelection)
+// return string "You Lose! Paper beats Rock" a = 'Rock', b = 'Paper', c = 'Scissors'
+// "You Lose! b > a"
+// return results of function
+// 
+// playerSelection must be case-insensitive 'rock' 'Rock' 'ROCK'
+// 
+// new function game(playRound) 5-round game , track score, report winner or loser
+// 
+// create Window.prompt(message, defaultValue) to get input from user
+// 
+
+// Rock Paper Scissors
+// HTML 
+// Submit page to Google Lighthouse
+// Use icons from Google or other icon site
+
+// CSS
+// Learn about BEM principles and incorporate
+// What animations could you incorporate?
+
+// Javascript 
+// Learn about and incorporate TypeScript
+// Research scope and hoisting for variables and functions. Global v local?
+// Function expression v function statement?
+// Think you can make a modal pop up to restart the game?
+// How difficult would local storage be to incorporate?
+// What benefits does it add?
+
+
+// React Practice
+// Could you use ternary (short circuit operators) here?
+// How would useState work here?
+// Can you find a way to use template string literals for this project?
